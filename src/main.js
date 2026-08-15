bd2d203fb99467881fbafbb82196fc1372dc8ec6
Mert Ulubay <mertulubay@gmail.com>
2026-08-15 12:58:35 +0300

    Update and rename main.js to src/main.js

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-import { createClient } from "@supabase/supabase-js";
+/import { createClient } from "@supabase/supabase-js";
 import "./style.css";
 
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
